346cd7395520e05a86f05ef58f6bef25a7ad0119
Mendy <mendy.edri@carlsonwagonlit.com>
2020-01-01 15:44:46 +0200

    Add RetryExecutor `retry` return value test

--- TDDChatProjectTests/Chat Framewotk Tests/RetryExecutorTests.swift
+++ TDDChatProjectTests/Chat Framewotk Tests/RetryExecutorTests.swift
@@ -52,6 +52,32 @@ class RetryExecutorTests: XCTestCase {
         XCTAssertTrue(counter == 2)
     }
     
+    func test_retry_notCalledAfterObjectHasBeenDeallocated() {
+        var counter = 0
+        var sut = RetryExecutor(attempts: 5) {
+            counter += 1
+        }
+        
+        sut?.retry()
+        sut = nil
+        sut?.retry()
+        sut?.retry()
+        
+        XCTAssertTrue(counter == 1)
+    }
+    
+    func test_retry_retryReturnValueFalseAfterMaxAttempts() {
+        var counter = 0
+        let sut = RetryExecutor(attempts: 2) {
+            counter += 1
+        }!
+        
+        sut.retry()
+        XCTAssertTrue(sut.retry())
+        
+        XCTAssertFalse(sut.retry())
+    }
+    
     // MARK: Helpers
     
     private func anyError() -> Error {
