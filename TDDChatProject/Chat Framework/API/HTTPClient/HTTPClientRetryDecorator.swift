c0a2eed2668e5428b01afa0b89194f9319fe811a
Mendy <mendy.edri@carlsonwagonlit.com>
2020-01-01 15:46:21 +0200

    Added `RetryExecutor` into `HTTPClientRetryDecorator` to have retry functionality in the decorator

--- TDDChatProject/Chat Framework/API/HTTPClient/HTTPClientRetryDecorator.swift
+++ TDDChatProject/Chat Framework/API/HTTPClient/HTTPClientRetryDecorator.swift
@@ -8,10 +8,11 @@
 
 import Foundation
 
-public struct HTTPClientRetryDecorator: HTTPClientDecorator {
+public class HTTPClientRetryDecorator: HTTPClientDecorator {
     
     private let httpClient: HTTPClient
     private let attempts: Int
+    private var retrier: RetryExecutor?
     
     init(http client: HTTPClient, attempts: Int) {
         self.httpClient = client
@@ -19,7 +20,27 @@ public struct HTTPClientRetryDecorator: HTTPClientDecorator {
     }
     
     public func get(from url: URL, method: HTTPMethod, headers: [String : String]?, body: [String : String]?, completion: @escaping (HTTPClientRetryDecorator.Result) -> Void) {
-        // ... todo
         
+        retrier = RetryExecutor(attempts: attempts) { [weak self] in
+            self?.clientCall(from: url, method: method, headers: headers, body: body, completion: completion)
+        }
+    }
+    
+    // Mark: Helpers
+    
+    private func clientCall(from url: URL, method: HTTPMethod, headers: [String: String]?, body: [String: String]?, completion: @escaping (HTTPClientRetryDecorator.Result) -> Void) {
+        
+        httpClient.get(from: url, method: method, headers: headers, body: body) { [weak self] result in
+            guard let self = self else { return }
+            
+            completion(self.handle(client: result))
+        }
+    }
+    
+    private func handle(client result: HTTPClientRetryDecorator.Result) -> HTTPClientRetryDecorator.Result {
+        if case let .failure(error) = result, retrier?.retry() == false {
+            return .failure(error)
+        }
+        return result
     }
 }
