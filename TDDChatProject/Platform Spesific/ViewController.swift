e85c825e8548eaedb8263d98fc0814a75c7ee3dc
Mendy Edri <mendy.edri@carlsonwagonlit.com>
2020-02-05 10:31:11 +0200

    Platform and Project specific changes

--- TDDChatProject/Platform Spesific/ViewController.swift
+++ TDDChatProject/Platform Spesific/ViewController.swift
@@ -11,30 +11,67 @@ import UIKit
 class ViewController: UIViewController {
     
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
-    
+    @IBOutlet weak var statusLabel: UILabel!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        activityIndicator.startAnimating()
         activityIndicator.hidesWhenStopped = true
-        
-        //buttonSelected()
-    }
-    
-    func buttonSelected() {
-        ChatDefaultComposition.manager.prepare { [weak self] result in
-            print("Done Prepare: \(result)")
-            DispatchQueue.main.async {
-                self?.activityIndicator.stopAnimating()
-            }
-        }
+        statusLabel.text = ""
     }
 
     @IBAction func didTouchOpenChat(_ sender: Any) {
-        if ChatDefaultComposition.manager.isReady() {
-            return
+        startAnimating()
+        Chat.shared.showWhenReady { [weak self] result in
+            self?.stopAnimating()
+            self?.statusLabel(from: result)
         }
-        self.activityIndicator.isHidden = false
-        self.activityIndicator.startAnimating()
+    }
+    
+    // MARK: Helpers
+    
+    private func statusLabel(from result: ClientMediator.ClientState) {
+        switch result {
+        case .ready:
+            statusLabel.text = "Chat is Ready"
+        case .notReady:
+            statusLabel.text = "Chat is Not Ready"
+        case let .failed(error):
+            statusLabel.text = "Chat Failed \(error)"
+        }
+    }
+    
+    private func startAnimating() {
+        activityIndicator.startAnimating()
+    }
+    
+    private func stopAnimating() {
+        activityIndicator.stopAnimating()
     }
 }
 
+/// facade of ClientMediator and ChatConversation
+final class Chat {
+    public static let shared = Chat()
+    
+    private var mediator: ClientMediator = ChatDefaultComposition.manager
+    private var conversation: ChatConversation = SmoochConversation()
+    
+    private init() {}
+    
+    func showConversation() {
+        conversation.showConversation()
+    }
+    
+    func prepare(_ completion: @escaping (ClientMediator.ClientState) -> Void) {
+        mediator.prepare(completion)
+    }
+    
+    func showWhenReady(_ completion: ((ClientMediator.ClientState) -> Void)? = nil) {
+        prepare { [weak self] result in
+            if self?.mediator.isReady() ?? false {
+                self?.showConversation()
+            }
+            completion?(result)
+        }
+    }
+}
